1f72ba1d1054310931ac628ad3b9e8a9e0a27b22
2005050213zhenglu <77777104+2005050213zhenglu@users.noreply.github.com>
2022-10-28 00:02:14 +0800

    Create 202209-1.cpp

---
+++ solution/202209-1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    vector<int> A(n);
+    vector<int> B(n);
+    for(int i = 0;i < n;i++) {
+        cin>>A[i];
+    }
+    vector<int> C(n+1);
+    C[0] = 1;
+    int sum = 0;
+    for(int i = 0;i < n;i++) {
+        C[i+1] = C[i] * A[i];
+        B[i] = (m % C[i+1] - sum) / C[i];
+        sum += B[i] * C[i];
+    }
+    for(int &b : B) {
+        cout<<b<<" ";
+    }
+    system("pause");
+    return 0;
+}
